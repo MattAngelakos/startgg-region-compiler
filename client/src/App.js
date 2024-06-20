822c16454d9047201404b668d18b61ef9891bff6
Matthew Angelakos <93948171+MattAngelakos@users.noreply.github.com>
2024-06-20 02:31:32 -0400

    more work for player
    more work for player

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    useParams,
+    Routes
+} from 'react-router-dom';
 
-function ProfilePage() {
-  let { playerId } = useParams();
-  return (
-    <div>
-      Profile Page for player with ID: {playerId}
-    </div>
-  );
-}
+const PlayerDetail = () => {
+    let { playerId } = useParams();
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/player/*">
-          <Route path=":playerId" element={<ProfilePage />} />
-          <Route path="stats" element={<div>Player Stats</div>} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-}
+    const [playerData, setPlayerData] = useState(null);
+
+    useEffect(() => {
+        const fetchPlayerData = async () => {
+            try {
+                const response = await fetch(`/player/${playerId}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch player data');
+                }
+                const data = await response.json();
+                setPlayerData(data.player);
+            } catch (error) {
+                console.error('Error fetching player data:', error);
+            }
+        };
+
+        fetchPlayerData();
+    }, [playerId]);
+
+    if (!playerData) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>Player Information: {playerData.gamerTag}</h1>
+            <p>id: {playerData._id}</p>
+            <h2>Games:</h2>
+            <ul>
+              {playerData.games.map((game, index) => (
+                  <li key={index}>
+                      <p>gameId: {game.gameId}</p>
+                  </li>
+              ))}
+            </ul>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <Router>
+          <Routes>
+              <Route path="/player/:playerId" element={<PlayerDetail />}></Route>
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
